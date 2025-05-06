c39c75b196f1e479e87dd8f74628ec7c5c72f253
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-05-06 22:24:40 +0300

    feat: allow database path configuration via environment variable

--- db/database.go
+++ db/database.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	// Import and initialize the SQLite driver
 	_ "github.com/mattn/go-sqlite3"
@@ -14,8 +15,15 @@ var DB *sql.DB
 // InitDB, initializes the database and creates the table
 func InitDB() {
 	var err error
+
+	// Get database path from environment variable or use default
+	dbPath := os.Getenv("DB_PATH")
+	if dbPath == "" {
+		dbPath = "./accounts.db"
+	}
+
 	// Connect to the SQLite database (creates the file if it doesn't exist)
-	DB, err = sql.Open("sqlite3", "./accounts.db")
+	DB, err = sql.Open("sqlite3", dbPath)
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
 	}
